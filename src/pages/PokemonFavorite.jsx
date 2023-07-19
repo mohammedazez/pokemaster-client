47dbea785d3138b2c5e2615ccac3ac2418bc0cd8
mohammedazez <muhamadaziz047@gmail.com>
2023-07-19 16:50:00 +0700

    add search by name

--- src/pages/PokemonFavorite.jsx
+++ src/pages/PokemonFavorite.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import pokemonAction from "../redux/actions/pokemonActions";
 import { setUpdatePokemonAction } from "../redux/actions/pokemonActions";
@@ -7,6 +7,7 @@ import Swal from "sweetalert2";
 
 const PokemonFavorite = () => {
   const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
 
   const pokemon = useSelector((state) => state.pokemon.dataPokemonFavorite);
   useEffect(() => {
@@ -40,6 +41,10 @@ const PokemonFavorite = () => {
         window.location.reload();
       }
     });
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
   };
 
   const Button = styled.button`
@@ -76,37 +81,64 @@ const PokemonFavorite = () => {
         </div>
       </div>
 
+      <div className="container-fluid src-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <label htmlFor="frm-search" className="form-label src-label">
+                <h3>Search by Name </h3>
+              </label>
+              <input
+                className="form-control form-control-lg"
+                id="frm-search"
+                type="text"
+                placeholder="Example Pineco"
+                onChange={handleSearch}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="container-fluid crd-container">
         <div className="container">
           <div className="row">
             {pokemon.data &&
-              pokemon.data.map((item, index) => {
-                return (
-                  <div className="col-md crd-col" key={index}>
-                    <div
-                      className="card poke-card"
-                      style={{ width: "18rem", height: "19rem" }}
-                    >
-                      <div className="img-container">
-                        <img
-                          src={item.pokemon_picture}
-                          className="card-img-top poke-img"
-                          alt="..."
-                        />
+              pokemon.data
+                .filter((item) => {
+                  return item === ""
+                    ? item
+                    : item.pokemon_name
+                        .toLowerCase()
+                        .includes(search.toLowerCase());
+                })
+                .map((item, index) => {
+                  return (
+                    <div className="col-md crd-col" key={index}>
+                      <div
+                        className="card poke-card"
+                        style={{ width: "18rem", height: "19rem" }}
+                      >
+                        <div className="img-container">
+                          <img
+                            src={item.pokemon_picture}
+                            className="card-img-top poke-img"
+                            alt="..."
+                          />
+                        </div>
+                        <div className="card-body">
+                          <h4 className="card-title">
+                            Nickname: {item.pokemon_name}
+                          </h4>
+                          <p className="card-title">Number: {item.number}</p>
+                        </div>
+                        <Button onClick={() => handleCatch(item.id)}>
+                          Update
+                        </Button>
                       </div>
-                      <div className="card-body">
-                        <h4 className="card-title">
-                          Nickname: {item.pokemon_name}
-                        </h4>
-                        <p className="card-title">Number: {item.number}</p>
-                      </div>
-                      <Button onClick={() => handleCatch(item.id)}>
-                        Update
-                      </Button>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
           </div>
         </div>
       </div>
